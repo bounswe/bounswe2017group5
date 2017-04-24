c0ded55003f55cd0bab5f4d273a1fb08f4ba6406
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-04-24 21:25:06 +0300

    Added UserView

--- gaussianApi/apiApp/views.py
+++ gaussianApi/apiApp/views.py
@@ -3,6 +3,14 @@ from apiApp.models import Profile, Comment
 from apiApp.serializers import ProfileSerializer, CommentSerializer
 from rest_framework import generics
 
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
 
 def index(request):
     return HttpResponse("Hello, group 5. This is our first version of API project with Django.")
+	

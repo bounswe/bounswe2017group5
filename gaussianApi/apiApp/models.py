da01deaede7373318b6571449d930e7ff8a7a4ee
gnl <aycignl@gmail.com>
2017-04-24 20:11:25 +0300

    added Comment model to project

--- gaussianApi/apiApp/models.py
+++ gaussianApi/apiApp/models.py
@@ -1,6 +1,10 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+	author = models.ForeignKey('auth.User', related_name='comment', on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+    	ordering = ('created',)

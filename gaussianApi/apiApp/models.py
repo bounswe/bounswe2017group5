ec07c74b3ea4e27c624597c8bb5503dfdc2ae7a6
gnl <aycignl@gmail.com>
2017-04-24 20:17:56 +0300

    fixed indentation

--- gaussianApi/apiApp/models.py
+++ gaussianApi/apiApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Comment(models.Model):
 	author = models.ForeignKey('auth.User', related_name='comment', on_delete=models.CASCADE)
-    text = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+	text = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
     	ordering = ('created',)

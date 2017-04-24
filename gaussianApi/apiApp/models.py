81656042455186c171c53ebc8cf386b67ff86b81
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-04-24 22:36:31 +0300

    Fixed model errors

--- gaussianApi/apiApp/models.py
+++ gaussianApi/apiApp/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
-     author = models.ForeignKey('auth.User', related_name='post', on_delete=models.CASCADE)
-     text = models.TextField()
-     created = models.DateTimeField(auto_now_add=True)
-      # a field for comments is needed	
-     class Meta:
-    	ordering = ('created',)
+	author = models.ForeignKey('auth.User', related_name='post', on_delete=models.CASCADE)
+	text = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	# a field for comments is needed	
+	class Meta:
+		ordering = ('created',)
 
 class Tag(models.Model):
 	created = models.DateTimeField(auto_now_add=True)

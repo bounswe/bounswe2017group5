1a8a20ccac60e14c371362f6a1ff67c1aaa732c5
irem <iremulutas1995@gmail.com>
2017-04-24 20:37:04 +0300

    Implemented Tag Model and Tag Serializer

--- gaussianApi/apiApp/models.py
+++ gaussianApi/apiApp/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Tag(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=30, blank=True, default='')
+
+	class Meta:
+		ordering = ('created',)
+
 # Create your models here.
--- gaussianApi/apiApp/serializers.py
+++ gaussianApi/apiApp/serializers.py
@@ -1,3 +1,6 @@
 from rest_framework import serializers
 
 # Create serializers here
+class TagSerializer(serializers.ModelSerializer):
+	model = Tag
+	fields = ('created', 'name')

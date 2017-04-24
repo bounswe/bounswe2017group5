feb14717df787b0b23bbe321aaaba9c0d4dec132
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-04-24 18:11:07 +0300

    Deleted old environment - We'll let everyone create their own environments - Added a gitignore to ignore env folder

--- gaussianApi/apiProject/settings.py
+++ gaussianApi/apiProject/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser'
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

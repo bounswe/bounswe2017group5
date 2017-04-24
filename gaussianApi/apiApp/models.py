6857c5fab63cdd695c85953742b3f64064dbe5bd
irem <iremulutas1995@gmail.com>
2017-04-24 19:59:14 +0300

    Implement group model and group serializer

--- gaussianApi/apiApp/models.py
+++ gaussianApi/apiApp/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Group(models.Model):
+	created = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=30, blank=True, default='')
+	# members field should be added as a one2many relation between group and user
+	isPublic = models.BooleanField(default = True)
+	description = models.TextField();
+	avatar = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+	location_lat = models.CharField(max_length = 10, blank=True)
+	location_lon = models.CharField(max_length = 10, blank=True)
+	#tags field should be added as a one2many relation between group and tags
+
+
+	class Meta:
+		ordering = ('created',)
+
+
+
 # Create your models here.
--- gaussianApi/apiApp/serializers.py
+++ gaussianApi/apiApp/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
 
 # Create serializers here
+
+class GroupSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Group
+		fields = ('created', 'name', 'isPublic', 'description' ,'avatar', 'location_lat', 'location_lat')
+

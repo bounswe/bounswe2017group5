4b23d39b45801bd8806099b0be9dd8480a778fc8
Mücahit Celik <mccelik@uni-bremen.de>
2017-04-24 21:38:24 +0300

    New Views for Comment and Tag

--- gaussianApi/apiApp/views.py
+++ gaussianApi/apiApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from apiApp.models import Profile, Comment
+from apiApp.models import Profile, Comment, Tag
 from apiApp.serializers import ProfileSerializer, CommentSerializer
 from rest_framework import generics
 
@@ -10,6 +10,23 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+	
+class CommentList(generics.ListAPIView)
+	queryset = Comment.object.all()
+	serializer_class = UserSerializer
+	
+class CommentDetail(generics.RetrieveAPIView):
+	queryset = Comment.object.all()
+	serializer_class = UserSerializer
+	
+class TagList(generics.ListAPIView)
+	queryset = Tag.object.all()
+	serializer_class = UserSerializer
+	
+class TagDetail(generics.RetrieveAPIView):
+	queryset = Tag.object.all()
+	serializer_class = UserSerializer
+	
 
 def index(request):
     return HttpResponse("Hello, group 5. This is our first version of API project with Django.")

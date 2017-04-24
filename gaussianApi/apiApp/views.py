1d4f0e47f03dc95374e1a4068c3d57b166b66b40
Berkay Özerbay <berkayozerbay@hotmail.com>
2017-04-24 21:46:56 +0300

    Added Group and Profile views

--- gaussianApi/apiApp/views.py
+++ gaussianApi/apiApp/views.py
@@ -13,20 +13,35 @@ class UserDetail(generics.RetrieveAPIView):
 	
 class CommentList(generics.ListAPIView)
 	queryset = Comment.object.all()
-	serializer_class = UserSerializer
+	serializer_class = CommentSerializer
 	
 class CommentDetail(generics.RetrieveAPIView):
 	queryset = Comment.object.all()
-	serializer_class = UserSerializer
+	serializer_class = CommentSerializer
 	
 class TagList(generics.ListAPIView)
 	queryset = Tag.object.all()
-	serializer_class = UserSerializer
+	serializer_class = TagSerializer
 	
 class TagDetail(generics.RetrieveAPIView):
 	queryset = Tag.object.all()
-	serializer_class = UserSerializer
+	serializer_class = TagSerializer
 	
+class GroupList(generics.ListAPIView)
+	queryset = Group.object.all()
+	serializer_class = GroupSerializer
+	
+class GroupDetail(generics.RetrieveAPIView):
+	queryset = Group.object.all()
+	serializer_class = GroupSerializer
+	
+class ProfileList(generics.ListAPIView)
+	queryset = Profile.object.all()
+	serializer_class = ProfileSerializer
+	
+class ProfileDetail(generics.RetrieveAPIView):
+	queryset = Profile.object.all()
+	serializer_class = ProfileSerializer
 
 def index(request):
     return HttpResponse("Hello, group 5. This is our first version of API project with Django.")

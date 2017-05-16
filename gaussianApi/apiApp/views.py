34159d65bbc4487810a3ad38863cb261ad1a33e4
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-05-16 23:37:04 +0300

    Rebased bugfix on to master

--- gaussianApi/apiApp/views.py
+++ gaussianApi/apiApp/views.py
@@ -13,15 +13,29 @@ from rest_framework.response import Response
 from rest_framework import authentication, permissions, status
 
 
-class UserList(generics.ListCreateAPIView):
+class UserList(generics.ListAPIView):
+	"""
+	Retrieve a list of users.
+	"""
+
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(generics.RetrieveAPIView):
+	"""
+	Retrieve information of a specific User.
+	"""
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 	
 class CommentList(generics.ListCreateAPIView):
+	"""
+	get:
+	Retrieve a list of Comments. Comments from private groups that the User has not joined, are hidden.
+
+	post:
+	Create a new Comment. Should have a parent post.
+	"""
 	serializer_class = CommentSerializer
 
 	def get_queryset(self):
@@ -34,9 +48,23 @@ class CommentList(generics.ListCreateAPIView):
 		serializer.save(author=self.request.user)
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	get:
+	Get details of a specific Comment.
+
+	put:
+	Edit an existing Comment that belongs to the authenticated User.
+
+	patch:
+	Edit an existing Comment that belongs to the authenticated User.
+
+	delete:
+	Delete an existing Comment that belongs to the authenticated User.
+	"""
 	permission_classes = [
 		IsOwnerOrReadOnly
 	]
+
 	serializer_class = CommentSerializer
 
 	def get_queryset(self):
@@ -46,14 +74,41 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
 		return comments
 	
 class TagList(generics.ListCreateAPIView):
+	"""
+	get:
+	Retrieve a list of Tags.
+
+	post:
+	Create a new Tag.
+	"""
 	queryset = Tag.objects.all()
 	serializer_class = TagSerializer
 	
 class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	get:
+	Get details of a specific Tag.
+
+	put:
+	Edit an existing Tag.
+
+	patch:
+	Edit an existing Tag.
+
+	delete:
+	Delete an existing Tag.
+	"""
 	queryset = Tag.objects.all()
 	serializer_class = TagSerializer
 	
 class GroupList(generics.ListCreateAPIView):
+	"""
+	get:
+	Retrieve a list of Groups. Groups that are private, are not visible.
+
+	post:
+	Create a new Group. Authenticated User will be the admin of the created group.
+	"""
 	permission_classes = [
 		permissions.IsAuthenticatedOrReadOnly
 	]
@@ -74,6 +129,19 @@ class GroupList(generics.ListCreateAPIView):
 	
 	
 class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	get:
+	Get details of a specific Group. Groups that are private will return 404 Not Found.
+
+	put:
+	Edit an existing Group. The authenticated user should be the admin of the Group.
+
+	patch:
+	Edit an existing Group. The authenticated user should be the admin of the Group.
+
+	delete:
+	Delete an existing Group. The authenticated user should be the admin of the Group.
+	"""
 	permission_classes = [
 		IsOwnerOrReadOnly,
 		permissions.IsAuthenticatedOrReadOnly
@@ -93,20 +161,41 @@ class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
 
 	def perform_create(self, serializer):
 		serializer.save(admin=self.request.user)
+		
 
-
-class ProfileList(generics.ListCreateAPIView):
+class ProfileList(generics.ListAPIView):
+	"""
+	get:
+	Retrieve a list of Profiles.
+	"""
 	queryset = Profile.objects.all()
 	serializer_class = ProfileSerializer
 
-	def perform_create(self, serializer):
-		serializer.save(user=self.request.user)
-
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+	"""
+	get:
+	Retrieve a specific Profile.
+
+	put:
+	Edit an existing Profile.
+
+	patch:
+	Edit an existing Profile.
+	"""
 	queryset = Profile.objects.all()
 	serializer_class = ProfileSerializer
   
 class PostList(generics.ListCreateAPIView):
+	"""
+	get:
+	Retrieve a list of Posts. Posts that are posted to a private 
+	Group are not visible unless the authenticated User is a member 
+	of the private Group.
+
+	post:
+	Create a Post on a Group. The authenticated User should be a
+	member of the Group.
+	"""
 	permission_classes = [
 		permissions.IsAuthenticatedOrReadOnly
 	]
@@ -122,10 +211,29 @@ class PostList(generics.ListCreateAPIView):
 		serializer.save(author=self.request.user)
 	
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+
+	"""
+	get:
+	Retrieve a specific Post. The Group of the Post must be public.
+	Or the authenticated User should be a member of this Group.
+
+	put:
+	Edit an existing Post. The authenticated User must be the
+	author of this Post.
+
+	patch:
+	Edit an existing Post. The authenticated User must be the
+	author of this Post.
+
+	delete:
+	Delete an existing Post. The authenticated User must be the
+	author of this Post.
+	"""
 	permission_classes = [
 		permissions.IsAuthenticatedOrReadOnly,
 		IsOwnerOrReadOnly
 	]
+
 	serializer_class = PostSerializer
 
 	def get_queryset(self):
@@ -135,6 +243,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 		return posts
 
 class RegisterView(generics.CreateAPIView):
+	"""
+	post:
+	Registers a User. Allowed for non-authenticated Users, too.
+
+	"""
 	permission_classes = [
 		permissions.AllowAny
 	]
--- gaussianApi/apiProject/urls.py
+++ gaussianApi/apiProject/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^', include('apiApp.urls')),
+    url(r'^docs/', include_docs_urls(title="SWEGroup5 API")),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]

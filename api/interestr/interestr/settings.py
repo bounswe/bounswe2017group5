f2785838af2e2adfc9d8470564d4c8f76e2b5378
Samed Duzcay <samedduzcay@gmail.com>
2017-10-19 14:46:18 +0300

    REST_FRAMEWORK settings array => dict

--- api/interestr/interestr/settings.py
+++ api/interestr/interestr/settings.py
@@ -42,12 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-REST_FRAMEWORK = [
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
-]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

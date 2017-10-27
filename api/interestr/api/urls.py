3ed5faff2acd42aaf5d463bdd8882bff1c56d5c2
Utkan Gezer <utkangezer@gmail.com>
2017-10-27 20:48:18 +0300

    Added details view for the DataTemplate.

--- api/interestr/api/urls.py
+++ api/interestr/api/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name='api'
 urlpatterns = [
     url(r'^users/$', views.UserList.as_view(), name='users'),
-    url(r'^data_templates/$', views.DataTemplateList.as_view(), name='data_templates'),
     url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view(), name='userdetail'),
+    
+    url(r'^data_templates/$', views.DataTemplateList.as_view(), name='datatemplates'),
+    url(r'^data_templates/(?P<pk>\d+)/$', views.DataTemplateDetail.as_view(), name='datatemplatedetail'),
 ]

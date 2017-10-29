baef094cdb0a2e21e5e9d9547937b6e0845aa266
Murat Karaman <muratkaraman@Murats-MacBook-Pro.local>
2017-10-29 15:29:44 +0300

    website/views.py updated

--- api/interestr/website/views.py
+++ api/interestr/website/views.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Group
+
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'templates/groups.html'
+
+    def get_queryset(self):
+        return Group.objects.all()

7ec6962dbd69e19ed03f5b07ad059dd4bf8bbc1a
Mahmut Karaca <mahmutkaraca95@gmail.com>
2017-10-31 19:07:07 +0300

    Added links, small modifications, added urls

--- api/interestr/interestr/urls.py
+++ api/interestr/interestr/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/docs/', include_docs_urls(title='Interestr API')),
@@ -24,4 +27,4 @@ urlpatterns = [
     url(r'^',include('website.urls')),
 
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- api/interestr/website/urls.py
+++ api/interestr/website/urls.py
@@ -6,8 +6,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
-    #views ayarlaninca buraya ekle.
-
+    url(r'^$', views.HomePageView.as_view(), name='index'),
     url(r'^login/$', views.UserLoginView.as_view(), name='login'),
     url(r'^register/$', views.UserRegisterView.as_view(), name='register'),
     url(r'^groups/$', views.GroupView.as_view(), name='groups'),

1571a6ec3926e24a9e59ba025bbbbf19f1d075fc
Morgazipa <sorusuz_cevaplar@hotmail.com>
2017-11-02 00:52:54 +0300

    Changed the styling of the  fields of the register form and fixed the bug after invalid input

--- api/interestr/website/forms.py
+++ api/interestr/website/forms.py
@@ -16,7 +16,10 @@ class LoginForm(forms.Form):
 
 class RegisterForm(UserCreationForm):
 
-	email = forms.EmailField(required=True)
+	username = forms.CharField(label="Username",widget = forms.TextInput(attrs={'placeholder': 'Username', 'class':"form-control"}))
+	password1 = forms.CharField(label="Password", widget = forms.PasswordInput(attrs={'placeholder': 'Password', 'class':"form-control"}))
+	password2 = forms.CharField(label="Confirm Password", widget = forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class':"form-control"}))
+	email = forms.EmailField(required=True,label="Email Address", widget = forms.EmailInput(attrs={'placeholder': 'Email Address', 'class':"form-control"}))
 
 	class Meta:
 		model = User

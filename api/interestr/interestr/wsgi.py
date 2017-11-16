5d8938afcb7eef38ea085a52d9ac1277bf226b68
Ubuntu <ubuntu@ip-172-31-0-157.eu-west-2.compute.internal>
2017-11-16 14:09:03 +0000

    Separate settings from the local version

--- api/interestr/interestr/wsgi.py
+++ api/interestr/interestr/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interestr.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interestr.prod_settings")
 
 application = get_wsgi_application()

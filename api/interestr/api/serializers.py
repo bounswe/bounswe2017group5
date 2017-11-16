df50a3030dc4f2aa052671981d9d5905b878a738
Samed Duzcay <samedduzcay@gmail.com>
2017-11-16 15:26:13 +0300

    add data templates and tags to the serializers

--- api/interestr/api/serializers.py
+++ api/interestr/api/serializers.py
@@ -3,26 +3,6 @@ from rest_framework import serializers
 from . import models as core_models
 from django.contrib.auth import models as auth_models
 
-class GroupSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = core_models.Group
-        fields = ('id', 'name', 'created', 'updated', 'size', 'members', 'moderators', 'picture', )
-
-class UserSerializer(serializers.ModelSerializer):
-    joined_groups = GroupSerializer(many=True, read_only=True)
-    moderated_groups = GroupSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = auth_models.User
-        fields = ('id', 'username', 'email', 'joined_groups', 'moderated_groups', )
-
-class PostSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = core_models.Post
-		fields = ('id', 'owner', 'text', 'group', 'data_template', 'created', 'updated', )
-
 class DataTemplateSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -33,4 +13,28 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = core_models.Tag
-        fields = ('id','label', 'url','description', 'concepturi', 'created', 'updated')
+        fields = ('id','label', 'url','description', 'concepturi', 'created', 'updated', 'groups' )
+
+class GroupSerializer(serializers.ModelSerializer):
+    data_templates = DataTemplateSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = core_models.Group
+        fields = ('id', 'name', 'created', 'updated', 'size', 'members', 'moderators', 'picture', 'data_templates',
+                  'tags', )
+
+class UserSerializer(serializers.ModelSerializer):
+    joined_groups = GroupSerializer(many=True, read_only=True)
+    moderated_groups = GroupSerializer(many=True, read_only=True)
+    data_templates = DataTemplateSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = auth_models.User
+        fields = ('id', 'username', 'email', 'joined_groups', 'moderated_groups', 'data_templates', )
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = core_models.Post
+        fields = ('id', 'owner', 'text', 'group', 'data_template', 'created', 'updated', )

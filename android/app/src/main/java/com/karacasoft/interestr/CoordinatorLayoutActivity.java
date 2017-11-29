52bedb1aad3b8bf8c807f35026a0d6bb1cea8e42
esra-aydemir <h.esraaydemir@hotmail.com>
2017-11-29 17:48:16 +0300

    coordinator activity toolbar default image added.

--- android/app/src/main/java/com/karacasoft/interestr/CoordinatorLayoutActivity.java
+++ android/app/src/main/java/com/karacasoft/interestr/CoordinatorLayoutActivity.java
@@ -4,7 +4,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.karacasoft.interestr.R;
 import com.karacasoft.interestr.pages.profile.ProfileFragment;
 
 public class CoordinatorLayoutActivity extends AppCompatActivity {
@@ -12,10 +11,10 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_coordinator_layout);
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction()
-                .replace(R.id.profile_activity_content, ProfileFragment.newInstance(0))
+                .replace(R.id.coordinator_layout_activity_content, ProfileFragment.newInstance(0))
                 .commit();
     }
 }
